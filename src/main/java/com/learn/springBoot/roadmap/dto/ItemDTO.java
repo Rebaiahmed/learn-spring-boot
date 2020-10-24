5db7ebd26868dffd979119b03bb1e85d4dc88939
ahmed.rebai <ahmed.rebai@be-softilys.tn>
2020-10-24 16:36:16 +0100

    Roadmap : Rest Template Client

---
+++ src/main/java/com/learn/springBoot/roadmap/dto/ItemDTO.java
@@ -0,0 +1,49 @@
+package com.learn.springBoot.roadmap.dto;
+
+public class ItemDTO {
+
+    private String userId;
+    private  String id;
+    private String title;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    private boolean completed;
+
+    public ItemDTO(String userId, String id, String title, boolean completed) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+}
---
+++ src/main/java/com/learn/springBoot/roadmap/web/rest/ExternalApiRessource.java
@@ -0,0 +1,39 @@
+package com.learn.springBoot.roadmap.web.rest;
+
+
+import com.learn.springBoot.roadmap.dto.ItemDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ExternalApiRessource {
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+    private static final String URL_API = "https://jsonplaceholder.typicode.com/todos";
+
+
+
+
+    @RequestMapping(value="/todos" , method = RequestMethod.GET)
+    public List<ItemDTO> getTodosList()
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        return restTemplate.exchange(URL_API, HttpMethod.GET, entity, List.class).getBody();
+
+    }
+}
