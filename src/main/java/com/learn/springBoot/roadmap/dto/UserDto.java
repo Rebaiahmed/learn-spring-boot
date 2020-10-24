98fcef1126208a0af5d8e879817134be6b6af9a7
ahmed.rebai <ahmed.rebai@be-softilys.tn>
2020-10-24 15:27:23 +0100

    Roadmap : Rest API v1

---
+++ src/main/java/com/learn/springBoot/roadmap/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.learn.springBoot.roadmap.dto;
+
+public class UserDto {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+}
---
+++ src/main/java/com/learn/springBoot/roadmap/web/rest/UserRessource.java
@@ -0,0 +1,43 @@
+package com.learn.springBoot.roadmap.web.rest;
+
+
+import com.learn.springBoot.roadmap.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserRessource {
+
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> getAllUsers() {
+        return new ResponseEntity<>("get all users success !", HttpStatus.OK);
+    }
+
+    @GetMapping("/all/filter")
+    public ResponseEntity<Object> searchUsers(@RequestParam(value = "username", required = false) String filter) {
+        return new ResponseEntity<>(String.format("search Users by filter  %s",filter), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createNewUser(@RequestBody UserDto newUser) {
+        return new ResponseEntity<>("User is created successfully v1", HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createUserV2(@RequestBody UserDto userDto) {
+        return new ResponseEntity<>("User is created successfully v2", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable("userId") String userId, @RequestBody UserDto updatedUser) {
+        return new ResponseEntity<>("User is updated successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUSER(@PathVariable("userId") String userId) {
+        return new ResponseEntity<>("User is updated successfully", HttpStatus.OK);
+    }
+}
